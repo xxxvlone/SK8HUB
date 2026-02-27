5ba13fd2374cd36078505c7871058dfe2e7da9d4
xxxvlone <you@example.com>
2026-02-27 21:35:03 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+const newsData = [
+  {
+    "title": "Mar.13th - FISE Battle of the Champions",
+    "link": "https://www.kickerclub.com/2026/02/mar-13th-fise-battle-of-the-champions/",
+    "score": 8,
+    "summary": "FISE街头霸主之争，纯正亚文化竞技。"
+  },
+  {
+    "title": "Nov.15th - Polar Shanghai Demo",
+    "link": "https://www.kickerclub.com/2025/11/nov-15th-polar-shanghai-demo/",
+    "score": 6,
+    "summary": "Polar品牌落地上海，街头滑板文化盛事预告。"
+  },
+  {
+    "title": "Oct.18th - DC Skate Night at Wuhan",
+    "link": "https://www.kickerclub.com/2025/10/oct-18th-dc-skate-night-at-wuhan/",
+    "score": 4,
+    "summary": "DC滑板夜，街头味浓，信息直白，缺乏冲击力。"
+  }
+];
